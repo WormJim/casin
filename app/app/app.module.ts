371e2cf4778b522f1f4a35fc34508a5b2c15cec5
Grahn Cooledge <gcooled@yahoo.com>
2018-04-20 23:55:27 -0400

    Added Components

--- app/app/app.module.ts
+++ app/app/app.module.ts
@@ -5,9 +5,17 @@ import { ClarityModule } from '@clr/angular';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { AuthComponent } from './components/auth/auth.component';
+import { AboutComponent } from './components/about/about.component';
+import { MissionComponent } from './mission/mission.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { BaseComponent } from './components/base/base.component';
+import { TeamComponent } from './components/team/team.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, AuthComponent],
+  declarations: [AppComponent, HomeComponent, AuthComponent, AboutComponent, MissionComponent, FooterComponent, HeaderComponent, ContactComponent, BaseComponent, TeamComponent, NavComponent],
   imports: [BrowserModule, ClarityModule],
   providers: [],
   bootstrap: [AppComponent]
